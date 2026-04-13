133e37ff382663f82b4887009d1e2ab6d2d6327c
drewski2step <drewwalters303@gmail.com>
2026-04-13 03:11:43 +0000

    revert: restore src to state before Restrict edit_history and Update site info commits

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-console.info('Substrate build 2026-04-13b');
 createRoot(document.getElementById("root")!).render(<App />);
